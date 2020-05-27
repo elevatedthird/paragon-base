b56ddb7ec06ad6764f588af95bbc7cb80604959a
afireintheattic <afireintheattic@3181619.no-reply.drupal.org>
2020-05-27 08:30:26 -0600

    Issue #3101358 by afireintheattic: Update config override path in settings.php

--- scaffold/example.settings.php
+++ scaffold/example.settings.php
@@ -1,13 +1,11 @@
 <?php
 
+//@condingStandardsIgnoreFile
+
 /**
  * Config settings
- * @TODO: Move to the repo root
  */
-
-$config_directories = array(
-  CONFIG_SYNC_DIRECTORY => '../config/default',
-);
+$settings['config_sync_directory'] = '../config/default';
 
 /**
  * Hash salt used for one-time login links, etc.
@@ -33,49 +31,13 @@ $settings['file_chmod_file'] = 0664;
 /**
  * Load services definition file.
  */
-$settings['container_yamls'][] = __DIR__ . '/services.yml';
-
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 
 /**
- * Trusted host configuration.
- *
- * Drupal core can use the Symfony trusted host mechanism to prevent HTTP Host
- * header spoofing.
- *
- * To enable the trusted host mechanism, you enable your allowable hosts
- * in $settings['trusted_host_patterns']. This should be an array of regular
- * expression patterns, without delimiters, representing the hosts you would
- * like to allow.
- *
- * For example:
- * @code
- * $settings['trusted_host_patterns'] = array(
- *   '^www\.example\.com$',
- * );
- * @endcode
- * will allow the site to only run from www.example.com.
- *
- * If you are running multisite, or if you are running your site from
- * different domain names (eg, you don't redirect http://www.example.com to
- * http://example.com), you should specify all of the host patterns that are
- * allowed by your site.
- *
- * For example:
- * @code
- * $settings['trusted_host_patterns'] = array(
- *   '^example\.com$',
- *   '^.+\.example\.com$',
- *   '^example\.org$',
- *   '^.+\.example\.org$',
- * );
- * @endcode
- * will allow the site to run off of all variants of example.com and
- * example.org, with all subdomains included.
+ * Trusted host patterns for e3develop and e3stanging. Make sure to add appropriate variations for production domain
+ * and any additional version thereof.
+ * Additional env specific patterns can be added in the following files (drupalvm, local)
  */
-
-// Trusted host patterns for e3develop and e3stanging. Make sure to add appropriate variations for production domain
-// and any additional version thereof.
-// Additional env specific patterns can be added in the following files (drupalvm, local)
 $settings['trusted_host_patterns'] = array(
   '^e3develop\.com$',
   '^.+\.e3develop\.com$',
@@ -83,45 +45,54 @@ $settings['trusted_host_patterns'] = array(
   '^.+\.e3staging\.com$',
 );
 
-// Set default paths to public, private and temp directories.
+/**
+ * Set default paths to public, private and temp directories.
+ */
 $settings['file_public_path'] = 'sites/default/files';
 $settings['file_private_path'] = '../private';
 $settings['file_temp_path'] = '../private/tmp';
 
-// Remove shield print message by default.
+/**
+ * Remove shield print message by default.
+ */
 $config['shield.settings']['print'] = '';
 
-// Allow cli to bypass shield.
+/**
+ * Allow cli to bypass shield.
+ */
 $config['shield.settings']['allow_cli'] = TRUE;
 
-// Set logging level default.
+/**
+ * Set logging level default.
+ */
 $config['system.logging']['error_level'] = 'all';
 
-// Set Google Analytics to NULL, override this for production environment.
-$config['']['account'] = '';
-
-// Disable dev modules on all environments by default.
-$config['config_split.config_split.config_dev']['status'] = FALSE;
-
-// Add fast404 settings
+/**
+ * Add fast404 settings
+ */
 if (file_exists($app_root . '/' . $site_path . '/settings.fast404.php')) {
   include $app_root . '/' . $site_path . '/settings.fast404.php';
 }
 
-// If $_ENV['AH_SITE_ENVIRONMENT'], load Acquia settings.
+/**
+ * If $_ENV['AH_SITE_ENVIRONMENT'], load Acquia settings.
+ */
 if(isset($_ENV['AH_SITE_ENVIRONMENT'])) {
-  if (file_exists(__DIR__ . '/settings.acquia.php')) {
-    include __DIR__ . '/settings.acquia.php';
-
+  if (file_exists($app_root . '/' . $site_path . '/settings.acquia.php')) {
+    include $app_root . '/' . $site_path . '/settings.acquia.php';
   }
 }
 
-// If drupal-vm settings exist, load them.
-elseif (file_exists(__DIR__ . '/settings.drupalvm.php')) {
-  include __DIR__ . '/settings.drupalvm.php';
+/**
+ * If drupal-vm settings exist, load them.
+ */
+elseif (file_exists($app_root . '/' . $site_path . '/settings.drupalvm.php')) {
+  include $app_root . '/' . $site_path . '/settings.drupalvm.php';
 }
 
-// If local settings file exists, load it.
-if(file_exists(__DIR__ . '/settings.local.php')) {
-  include __DIR__ . '/settings.local.php';
+/**
+ * If local settings file exists, load it.
+ */
+if(file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
 }
