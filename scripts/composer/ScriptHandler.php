0a89862a4d2c4dad04287c77a50fa0180147f933
Eric Schmidt <eschmidt@elevatedthird.com>
2021-05-11 13:12:43 -0600

    Ensures .git directories are removed from /docroot/libraries directory.

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -39,6 +39,7 @@ class ScriptHandler {
 
   public static function removeGitSubmodules (Event $event) {
     exec("find " . getcwd() . "'/vendor' -not -path \"*geerlingguy/drupal-vm*\" | grep '.git$' | xargs rm -rf");
+    exec("find " . getcwd() . "'/docroot/libraries' | grep '.git$' | xargs rm -rf");
     exec("find " . getcwd() . "'/docroot/modules/contrib' | grep '.git$' | xargs rm -rf");
     exec("find " . getcwd() . "'/docroot/profiles/contrib' | grep '.git$' | xargs rm -rf");
     exec("find " . getcwd() . "'/docroot/themes/contrib' | grep '.git$' | xargs rm -rf");
